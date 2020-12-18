900fb6c9a9748d08fdeb91dfa3676536561a37d5
neverlove936-git <67462670+neverlove936-git@users.noreply.github.com>
2020-12-18 15:39:44 +0700

    Update function.js

--- function.js
+++ function.js
@@ -3,6 +3,7 @@
  * @param {string} refreshToken
  */
 function sendData(accessToken, refreshToken) {
+  alert("Bazinga!!!  you called letMeCallYou");
   // In order to call window.flutter_inappwebview.callHandler(handlerName <String>, ...args)
   // properly, you need to wait and listen the JavaScript event flutterInAppWebViewPlatformReady.
   // This event will be dispatched as soon as the platform (Android or iOS) is ready to handle the callHandler method.
